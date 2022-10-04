28eb7808884962ea53b3362db660f740f2424189
Scott Murphy <scottymurph7@gmail.com>
2022-10-04 15:38:58 -0400

    Lab 4

--- Animation-Clips/Assets/Scripts/LerpScript.cs
+++ Animation-Clips/Assets/Scripts/LerpScript.cs
@@ -6,7 +6,15 @@ using UnityEngine.UI;
 public class LerpScript : MonoBehaviour
 {
     public Slider slider;
-    public Material mat;
+    Material mat;
+    float t;
+
+    int currentPoint = 0;
+    int nextPoint = 1;
+
+    public List<Transform> points;
+
+    public float speed;
 
     Color lerpColour;
 
@@ -19,5 +27,28 @@ public class LerpScript : MonoBehaviour
     {
         lerpColour = Color.Lerp(Color.white, Color.red, slider.value);
         mat.color = lerpColour;
+
+        t += Time.smoothDeltaTime / speed;
+
+        transform.position = Vector3.Lerp(points[currentPoint].position, points[nextPoint].position, t);
+
+        if (t > 1)
+        {
+            t = 0;
+
+            nextPoint++;
+            currentPoint++;
+
+            if (nextPoint > points.Count - 1)
+            {
+                nextPoint = 0;
+            }
+            if (currentPoint > points.Count - 1)
+            {
+                currentPoint = 0;
+            }
+        }
+
+        transform.Rotate(Vector3.one / 100);
     }
 }
